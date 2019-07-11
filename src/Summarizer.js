db68b2803a1db2030ed2ac858a6c5282fac407ad
zbot473 <zbot473@gmail.com>
2019-07-11 12:51:37 -0600

    Add an array option to the summary

--- src/Summarizer.js
+++ src/Summarizer.js
@@ -27,7 +27,7 @@ class Summarizer{
 	}
 
 	//Takes in a list of sorted sentences and a map of those sentences to the original sentences. 
-	listToString(sorted_sentences, clean_sentences){
+	summaryToString(sorted_sentences, clean_sentences){
 		const self = this;
 		let result_string = "";
 		let length_count = 0;
@@ -43,6 +43,22 @@ class Summarizer{
 		return result_string;
 	}
 
+	summaryToArray(sorted_sentences, clean_sentences){
+		const self = this;
+		let result_array = [];
+		let length_count = 0;
+		let count = self.number_of_sentences;
+		if(sorted_sentences.length < self.number_of_sentences){
+			count = sorted_sentences.length;
+		}
+		for(var i=0; i<count; i++){
+			length_count += sorted_sentences[i][1].split(" ").length;
+			result_array.push(clean_sentences[1].get(sorted_sentences[i][1]));
+		}
+		this.new_length = length_count;
+		return result_array;
+	}
+
 	summarizeByFrequency(){
 		const self = this
 		const list_to_clean = self.preprocesser.paragraphToSentences(self.string_to_process);
@@ -53,7 +69,7 @@ class Summarizer{
 		const sorted_sentences = self.sortSentences(sentence_weights_list);
 		
 		return {
-			summary: self.listToString(sorted_sentences, clean_sentences),
+			summary: self.summaryToString(sorted_sentences, clean_sentences),
 			sentence_list: list_to_clean,
 			weighted_map: weighted_map,
 			sorted_sentences: sorted_sentences
@@ -71,7 +87,8 @@ class Summarizer{
 			let text_rank_list = self.sortSentences(self.textRankMapToList(text_rank_map));
 			//let list_to_pass_in = text_rank_list;
 			return {
-				summary: self.listToString(text_rank_list, clean_sentences),
+				summaryString: self.summaryToString(text_rank_list, clean_sentences),
+				summaryArray: self.summaryToArray(text_rank_list, clean_sentences),
 				sentence_list: list_to_clean,
 				nouns_and_adjactive_map: nouns_and_adjactive_map
 			}
